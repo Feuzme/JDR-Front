0a272eb5f287c7d1a98187e2d110760a423960d9
Feuzme <feuzme@gmail.com>
2021-05-14 13:05:37 +0200

    architecture création des différent dossiers composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import {CardModule} from 'primeng/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './nav-bar/navbar/navbar.component';
+import { LoginComponent } from './pages/account/login/login.component';
+import { SignupComponent } from './pages/account/signup/signup.component';
+import { UserComponent } from './pages/account/user/user.component';
+import { UsersComponent } from './pages/account/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    UserComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
