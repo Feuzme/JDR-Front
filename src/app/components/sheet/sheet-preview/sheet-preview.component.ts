395467c8395b06dd831ec240a9c14fb6df83687f
Fyfeu Feuzme <feuzme@gmail.com>
2021-05-27 17:46:04 +0200

    feature(sheet-preview)calling the grid parameter from the layout-service

--- src/app/components/sheet/sheet-preview/sheet-preview.component.ts
+++ src/app/components/sheet/sheet-preview/sheet-preview.component.ts
@@ -7,15 +7,17 @@ import { LayoutService } from '../../services/sheet/layout.service'
   styleUrls: ['./sheet-preview.component.css']
 })
 export class SheetPreviewComponent implements OnInit {
-  // get options(): GridsterConfig {
-  //   return this.
-  // }
-  
-  dashboard: Array<GridsterItem>;
-  layout: GridsterItem[] = [];
+  get options(): GridsterConfig {
+    return this.layoutService.options
+  }
+  get dashboard(): GridsterItem[]{
+    return this.layoutService.layout
+  }
 
-  options: GridsterConfig = {};
-  constructor() { }
+  
+  constructor(private layoutService : LayoutService) {
+    
+   }
 
   static itemChange(item, itemComponent) {
     console.info('itemChanged', item, itemComponent);
@@ -26,24 +28,24 @@ export class SheetPreviewComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.options = {
-      // itemChangeCallback: SheetPreviewComponent.itemChange,
-      // itemResizeCallback: SheetPreviewComponent.itemResize,
-      gridType: GridType.Fit,
-      displayGrid: DisplayGrid.Always,
-      pushItems: true,
-      pushDirections: { north: true, east: false, south: true, west: true },
-      pushResizeItems: true,
-      swap: false,
-      draggable: {
-        enabled: true
-      },
-      resizable: {
-        enabled: true
-      },
-      disableWindowResize: true,
-      scrollToNewItems:false
-    };
+    // this.options = {
+    //   // itemChangeCallback: SheetPreviewComponent.itemChange,
+    //   // itemResizeCallback: SheetPreviewComponent.itemResize,
+    //   gridType: GridType.Fit,
+    //   displayGrid: DisplayGrid.Always,
+    //   pushItems: true,
+    //   pushDirections: { north: true, east: false, south: true, west: true },
+    //   pushResizeItems: true,
+    //   swap: false,
+    //   draggable: {
+    //     enabled: true
+    //   },
+    //   resizable: {
+    //     enabled: true
+    //   },
+    //   disableWindowResize: true,
+    //   scrollToNewItems:false
+    // };
     
     this.dashboard = [
       { cols: 2, rows: 1, y: 2, x: 2 },
