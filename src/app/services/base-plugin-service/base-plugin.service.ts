173edb18c32ded7f0c10cb2d5fc6e73aba2de67e
unknown <btlknajib@gmail.com>
2021-06-07 09:10:01 +0200

    service crud basePlugin

--- src/app/services/base-plugin-service/base-plugin.service.ts
+++ src/app/services/base-plugin-service/base-plugin.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { BasePlugIn } from 'src/app/models/BasePlugin';
 
 @Injectable({
@@ -8,7 +10,27 @@ export class BasePluginService {
 
   public basePlugins: BasePlugIn[] = [];
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  getAll = () : Observable<BasePlugIn[]> => {
+    return this.http.get<BasePlugIn[]>("http://localhost:8080/base-plugins"); 
+  }
+
+  getById = (id : number) : Observable<BasePlugIn> => {
+    return this.http.get<BasePlugIn>(`http://localhost:8080/base-plugins/${id}`);
+  }
+
+  update = (basePlugin : BasePlugIn) : Observable<BasePlugIn> => {
+    return this.http.put<BasePlugIn>(`http://localhost:8080/base-plugins`, basePlugin)
+  }
+
+  create = (basePlugin : BasePlugIn) : Observable<BasePlugIn> => {
+    return this.http.post<BasePlugIn>(`http://localhost:8080/base-plugins`, basePlugin);
+  }
+
+  delete = (basePlugin : BasePlugIn) : Observable<BasePlugIn> => {
+    return this.http.request<BasePlugIn>('delete', `http://localhost:8080/base-plugins`, { body : basePlugin});
+  }
 
   addBasePlugin(basePlugin: BasePlugIn) {
     this.basePlugins.push(basePlugin);
