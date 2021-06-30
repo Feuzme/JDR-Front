8580927dc56b1e1b7da17be4f39eecdaa730af24
Fyfeu Feuzme <feuzme@gmail.com>
2021-06-30 17:12:14 +0200

    fixbefore mergin

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,7 +3,7 @@ import { HttpHeaders, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { User } from '../models/user';
+import { User } from '../models/User';
 
 @Injectable({
   providedIn: 'root'
