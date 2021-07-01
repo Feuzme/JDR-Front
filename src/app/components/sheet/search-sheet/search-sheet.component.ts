3b8363a40f9d3e369cdfab71ae0eabd95cf4d709
Fyfeu Feuzme <feuzme@gmail.com>
2021-07-01 16:36:57 +0200

    feat(sheet search) listing model sheet

--- src/app/components/sheet/search-sheet/search-sheet.component.ts
+++ src/app/components/sheet/search-sheet/search-sheet.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ModelSheet } from './../../../models/ModelSheet';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-sheet',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchSheetComponent implements OnInit {
 
-  constructor() { }
+  modelSheets: ModelSheet[] = [
+    new ModelSheet("1", "test1", true, {id:"userId1"}, []),
+    new ModelSheet("2", "test2", true, {id:"userId2"}, []),
+    new ModelSheet("3", "test3", true, {id:"userId3"}, [])
+  ];
+
+  constructor() {}
 
   ngOnInit(): void {
+    
   }
 
 }
