826dad6d0ca08a9a874bf05d8361ecb8f05b7166
Amir <slampas@hotmail.fr>
2021-07-13 11:48:31 +0200

    Login et Inscription Ok - reste à bloquer les mots de passes

--- src/app/pages/account/resume-page/resume-page.component.ts
+++ src/app/pages/account/resume-page/resume-page.component.ts
@@ -1,26 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/models/user';
-import { UserService } from 'src/app/services/user.service';
+import {Component, OnInit} from '@angular/core';
+import {User} from 'src/app/models/user';
+import {UserService} from 'src/app/services/user.service';
 
 @Component({
-  selector: 'app-resume-page',
-  templateUrl: './resume-page.component.html',
-  styleUrls: ['./resume-page.component.css']
+    selector: 'app-resume-page',
+    templateUrl: './resume-page.component.html',
+    styleUrls: ['./resume-page.component.css']
 })
 export class ResumePageComponent implements OnInit {
 
- 
 
-  utilisateurId:string;
-  user:User;
-  constructor(private service: UserService) { }
+    utilisateurId: string;
+    user: User;
 
-  ngOnInit(): void {
-    this.utilisateurId = localStorage.getItem("utilisateurId");
-    this.service.findByIdDetail(this.utilisateurId).subscribe((user:User)=>{
-      this.user = user;
-    })
-  }
+    constructor(private service: UserService) {
+    }
+
+    ngOnInit(): void {
+        this.utilisateurId = localStorage.getItem('utilisateurId');
+        this.service.findByIdDetail(this.utilisateurId).subscribe((user: User) => {
+            this.user = user;
+        });
+    }
 
 
 }
--- src/app/pages/account/signup/signup.component.ts
+++ src/app/pages/account/signup/signup.component.ts
@@ -1,77 +1,113 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { Password } from 'primeng/password';
-import { AuthHttpService } from 'src/app/services/auth-http.service';
-import { UserService } from 'src/app/services/user.service';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AuthHttpService} from 'src/app/services/auth-http.service';
+import {UserService} from 'src/app/services/user.service';
+import {Message} from 'primeng/api';
 
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
 
-  form:FormGroup;
-  signupForm : FormGroup ;
-  cutilisation: any[] = [];
+    form: FormGroup;
+    signupForm: FormGroup;
+    cutilisation: any[] = [];
+    msgs: Message[];
+    isExist: boolean;
 
-  constructor(
-    private serviceC:AuthHttpService,
-    private service : UserService,
-    private fb : FormBuilder,
-    private router : Router
-  ) { 
-    this.signupForm = this.fb.group({
-      nom : "",
-      password : ""
-    })
-  }
+    constructor(
+        private serviceC: AuthHttpService,
+        private service: UserService,
+        private fb: FormBuilder,
+        private router: Router
+    ) {
+        this.signupForm = this.fb.group({
+            nom: '',
+            email: '',
+            password: '',
+            repassword: '',
+            cutilisation: ''
+        });
+    }
 
-  ngOnInit(): void {
-    this.form = new FormGroup({
-      nom: new FormControl("", Validators.required),
-      email:new FormControl("", Validators.required),
-      password: new FormControl("", Validators.required),
-      
-    })
-  }
-
-
-  ajouter = () => {
-   
-      this.router.navigate(["/home"]);
-    
-  }
-  
-
+    ngOnInit(): void {
+        this.form = new FormGroup({
+            nom: new FormControl('', Validators.required),
+            email: new FormControl('', Validators.required),
+            password: new FormControl('', Validators.required),
+            repassword: new FormControl('', Validators.required),
+            cutilisation: new FormControl(''),
+        });
+        this.isExist = false;
+    }
 
 
-  onSubmit(){
-    if(this.form.valid)
-      this.service.create(this.form.value).subscribe(user=>{
-      this.serviceC.connexion(this.form.value).subscribe((utilisateurId:string)=>{
-        localStorage.setItem("utilisateurId",utilisateurId);
-        window.location.href ="users/resume";
-      })  
-      }, (err)=>{});
+    ajouter = () => {
+        this.router.navigate(['/home']);
+    }
 
 
+    onSubmit() {
+        if (this.form.valid) {
+            if (this.form.value.password === this.form.value.repassword) {
+                this.service.create(this.form.value).subscribe(user => {
+                    if (user != null) {
+                        const dto = {
+                            email: user.email,
+                            nom: user.nom,
+                            password: user.password,
+                        };
+                        this.serviceC.connexion(dto).subscribe((utilisateurId: string) => {
+                            localStorage.setItem('utilisateurId', utilisateurId);
+                            window.location.href = 'users/resume';
+                        });
+                    }else{
+                        this.msgs = [{
+                            severity: 'error',
+                            summary: 'Erreur',
+                            detail: 'Utilisateur enregistré'
+                        }];
+                        this.isExist = true;
+                    }
+                }, (err) => {
+                    this.msgs = [{
+                        severity: 'error',
+                        summary: 'Erreur',
+                        detail: 'Une erreur est survenu lors de l\'enregistrement'
+                    }];
+                });
+            } else {
+                this.msgs = [{
+                    severity: 'error',
+                    summary: 'Erreur',
+                    detail: 'Vos mots de passe ne sont pas identiques'
+                }];
+            }
+        } else {
+            this.msgs = [{
+                severity: 'error',
+                summary: 'Erreur',
+                detail: 'Vérifiez vos champs'
+            }];
+        }
 
-    
-  }
+
+    }
 
 
-/*
-onSubmit(){
-  if(this.form.valid)
-      this.service.create(this.form.value).subscribe(user=>{
-        this.router.navigate(["users/resume"])
-      })
-    else
-      alert("Le formulaire n'est pas valide")
-      }
-*/
+    /*
+    onSubmit(){
+      if(this.form.valid)
+          this.service.create(this.form.value).subscribe(user=>{
+            this.router.navigate(["users/resume"])
+          })
+        else
+          alert("Le formulaire n'est pas valide")
+          }
+    */
 
 }
