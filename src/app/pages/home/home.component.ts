7743d62c7ba81235b16df44fdd5bcf583fc7518a
Fyfeu Feuzme <feuzme@gmail.com>
2021-07-14 16:40:59 +0200

    feat(home) carousel sheet

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,4 @@
-import { GameType } from 'src/app/models/GameType';
 import { ModelSheetHttpService } from './../../services/sheet/model-sheet-http.service';
-import { ModelSheet } from './../../models/ModelSheet';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -13,14 +11,19 @@ export class HomeComponent implements OnInit {
 
   monImage: string = "../../../assets/images/image1.jpg";
 
-  jeux : any = []
+  jeux: any = [
+    { nom: "donjon", image: "image1.jpg", fiche: "fiche1.jpg" },
+    { nom: "donjon", image: "image2.png", fiche: "fiche1.jpg" },
+    { nom: "donjon", image: "image2.png", fiche: "fiche1.jpg" },
+    { nom: "donjon", image: "image2.png", fiche: "fiche1.jpg" }
+  ]
 
   responsiveOptions;
 
   constructor(
     private router: Router,
-    private sheetHttpService : ModelSheetHttpService
-    ) {
+    private sheetHttpService: ModelSheetHttpService
+  ) {
     this.responsiveOptions = [
       {
         breakpoint: '1024px',
@@ -42,7 +45,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.loadSheet();
-   }
+  }
 
   newGame = () => {
     localStorage.removeItem("gameId");
@@ -53,15 +56,14 @@ export class HomeComponent implements OnInit {
     this.router.navigate(['/sheet-creation']);
   }
 
-  loadSheet(){
+  loadSheet() {
     this.sheetHttpService.getAll().subscribe(
-      (resp : any) => {
-        
-        for(let fiche of resp){
+      (resp: any) => {
+        for (let fiche of resp) {
           this.jeux.push(
             {
-              nom: fiche.nom, 
-              image: fiche.gameType.logo, 
+              nom: fiche.nom,
+              image: fiche.gameType.logo,
               fiche: fiche.gameType.logo
             }
           );
